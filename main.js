dda50101a4df08b2dc07db336b2119c1e3b51cd7
Alvi <alvidervishaj9@gmail.com>
2021-01-26 16:05:53 +0100

    Console Logged port when app started

--- main.js
+++ main.js
@@ -8,14 +8,16 @@ app.use(serveStatic(__dirname + "/dist"));
 
 app.use(cors());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(bodyParser.json());
 
 app.get(/.*/, (req, res) => {
-    res.sendFile(__dirname + "/dist/index.html")
-  });
+  res.sendFile(__dirname + "/dist/index.html")
+});
 
 var port = process.env.PORT || 5000;
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`App started on port : ${port}`)
+})
